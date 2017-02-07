fdbc2672f7c12f195eee7f180f84b69c96462837
Leszek Pawlak <leszek@bitnoi.se>
2017-02-07 17:25:38 +0100

    fix deploy

--- shipitfile.js
+++ shipitfile.js
@@ -1,3 +1,4 @@
+/* eslint max-len: ["error", 1000] */
 module.exports = function (shipit) {
   shipit.initConfig({
     production: {
@@ -6,7 +7,7 @@ module.exports = function (shipit) {
   })
 
   shipit.task('deploy', function () {
-    return shipit.local('scp dist.tar.gz lesyek@s4.mydevil.net:/home/lesyek/domains/itenteges.pl/.').then(function () {
+    return shipit.local('curl --ftp-create-dirs -T dist.tar.gz -u f1101_itenteges:GR6DmL7X6rws ftp://s4.mydevil.net/').then(function () {
       return shipit.remote('cd /home/lesyek/domains/itenteges.pl/public_nodejs/public && rm -rf * && tar -zxvf ../../dist.tar.gz')
     })
   })
